94321c892b288519cc28f334fb3ea9c288f4a9b4
Trivii1457 <trivi1457@gmail.com>
2026-06-08 16:50:09 -0500

    First Commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Torre.c
+ *
+ * Created: 6/8/2026 3:06:38 PM
+ * Author : USUARIO
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
